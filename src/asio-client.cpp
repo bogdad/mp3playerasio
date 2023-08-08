9a0182da81bd4255961834ea66f0482a160dc5fd
Vladimir Shakhov <vladimir@spotify.com>
2023-08-08 20:38:12 +0200

    asio

--- src/asio-client.cpp
+++ src/asio-client.cpp
@@ -1,8 +1,17 @@
 #include <asio.hpp>
+#include <asio/error.hpp>
+#include <asio/registered_buffer.hpp>
+#include <chrono>
+#include <cstdlib>
 #include <iostream>
+#include <ostream>
+#include <sstream>
+#include <string_view>
+#include <thread>
 
 using asio::ip::tcp;
 int main(int argc, char *argv[]) {
+  std::srand(std::time(nullptr));
   try {
     if (argc != 2) {
       std::cerr << "Usage: client <host>" << std::endl;
@@ -11,20 +20,38 @@ int main(int argc, char *argv[]) {
     asio::io_context io_context;
     tcp::resolver resolver(io_context);
     tcp::resolver::results_type endpoints =
-        resolver.resolve(argv[1], "daytime");
+        resolver.resolve(argv[1], "8060");
     tcp::socket socket(io_context);
     asio::connect(socket, endpoints);
     for (;;) {
-      std::array<char, 128> buf;
+      std::array<char, 128> buf{};
       asio::error_code error;
 
       size_t len = socket.read_some(asio::buffer(buf), error);
-      if (error == asio::error::eof)
-        break; // Connection closed cleanly by peer.
-      else if (error)
+      if (error == asio::error::eof) {
+        std::cout << "eof!" << std::endl;
+        break;
+      }
+      std::cout << "finished reading " << std::string_view(buf.data(), len) << std::endl;
+      if (error)
         throw asio::system_error(error); // Some other error.
+      int rand_delay = std::rand() % 15;
+      std::cout << "sleeping for " << rand_delay << std::endl;
+      
+      std::this_thread::sleep_for(std::chrono::seconds(rand_delay));
+
+      std::stringstream str;
+      str << "we reply this to it " << rand_delay;
+      std::string strstr = str.str();
+      auto nwrote = socket.write_some(asio::buffer(strstr.c_str(), strstr.length() + 1), error);
+      std::cout << "write error: " << error << std::endl;
+      if (error == asio::error::eof) {
+        std::cout << "eof!" << std::endl;
+        break;
+      } 
+      std::cout << "wrote " << nwrote << " of " << strstr << std::endl;
     }
   } catch (std::exception &e) {
-    std::cerr << e.what() << std::endl;
+    std::cout << "ex: " << e.what() << std::endl;
   }
 }
--- src/asio-server.cpp
+++ src/asio-server.cpp
@@ -1,6 +1,32 @@
+//
+// server.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2023 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #include <asio.hpp>
+#include <asio/basic_streambuf.hpp>
+#include <asio/buffer.hpp>
+#include <asio/detail/string_view.hpp>
+#include <asio/error.hpp>
+#include <asio/error_code.hpp>
+#include <asio/io_context.hpp>
+#include <asio/read.hpp>
+#include <asio/read_until.hpp>
+#include <asio/steady_timer.hpp>
+#include <asio/streambuf.hpp>
+#include <atomic>
+#include <ctime>
+#include <future>
 #include <iostream>
+#include <memory>
 #include <ostream>
+#include <string>
+#include <string_view>
 
 using asio::ip::tcp;
 
@@ -10,20 +36,147 @@ std::string make_daytime_string() {
   return ctime(&now);
 }
 
-int main(int argc, char **argv) {
+std::atomic_int should_stop = 0;
+
+void my_handler(int s) {
+  should_stop = 1;
+}
+
+class infinite_timer {
+public:
+  static constexpr auto interval = asio::chrono::seconds(10);
+  infinite_timer(asio::io_context &io_context): timer_(io_context, interval) {start();}
+private:
+  void start() {
+    timer_.expires_at(timer_.expires_at() + interval);
+    timer_.async_wait([this](const asio::error_code &error){
+      std::cout << "timer! " << make_daytime_string() << std::endl;
+      this->start();
+    });
+  }
+  asio::steady_timer timer_; 
+};
+
+class tcp_connection : public std::enable_shared_from_this<tcp_connection> {
+public:
+  static constexpr auto interval = asio::chrono::seconds(5);
+  typedef std::shared_ptr<tcp_connection> pointer;
+
+  static pointer create(asio::io_context &io_context) {
+    return pointer(new tcp_connection(io_context), [](tcp_connection *conn){
+      std::cout << "deleting " << conn << std::endl;
+      delete conn;});
+  }
+
+  tcp::socket &socket() { return socket_; }
+
+  void start() {
+    message_ = make_daytime_string();
+
+    auto ptr = shared_from_this();
+    asio::async_write(socket_, asio::buffer(message_),
+        [ptr](const asio::error_code &error, size_t bytes_transferred) {
+          ptr->handle_write(error, bytes_transferred);
+        }
+      );
+  }
+
+private:
+  tcp_connection(asio::io_context &io_context) : socket_(io_context), timer_(io_context), buff_(1024) {}
+
+  void handle_write(const asio::error_code &error,
+                    size_t /*bytes_transferred*/) {
+    if (!error) {
+      std::cout << "staring reading from the client" << std::endl;
+      auto self = shared_from_this();
+      timer_.expires_from_now(interval);
+      timer_.async_wait([self](const asio::error_code &){
+        std::cout << "timeout timer fired" << std::endl;
+        self->was_timeout_ = true;
+        self->socket_.shutdown(asio::socket_base::shutdown_both);
+        self->socket_.close();
+      });
+      asio::async_read_until(socket_, buff_, '\n', [self](const asio::error_code err, size_t read_size) {
+        if (err == asio::error::operation_aborted) {
+          std::cout << "read from client timeout" << std::endl;
+          return;
+        }
+        using asio::streambuf;
+        size_t s = 0;
+        streambuf::const_buffers_type bufs = self->buff_.data();
+        streambuf::const_buffers_type::const_iterator i = bufs.begin();
+        std::cout << "read from client: "; 
+        while (i != bufs.end()) {
+          asio::const_buffer buf(*i++);
+          std::cout << std::string_view(static_cast<const char *>(buf.data()), bufs.size());
+          s += buf.size();
+        }
+        std::cout << std::endl;
+        self->was_timeout_ = false;
+        self->timer_.cancel();
+      });
+    }
+  }
+  tcp::socket socket_;
+  std::string message_;
+  asio::streambuf buff_;
+  char delim_ = '\0';
+  asio::steady_timer timer_;
+  bool was_timeout_{false};
+};
+
+class tcp_server {
+public:
+  tcp_server(asio::io_context &io_context)
+      : io_context_(io_context),
+        acceptor_(io_context, tcp::endpoint(tcp::v4(), 8060)) {
+    start_accept();
+  }
+
+private:
+  void start_accept() {
+    tcp_connection::pointer new_connection =
+        tcp_connection::create(io_context_);
+    acceptor_.async_accept(new_connection->socket(),
+      [this, new_connection](const asio::error_code &error) {
+        this->handle_accept(new_connection, error);
+      }
+    );
+  }
+
+  void handle_accept(tcp_connection::pointer new_connection,
+                     const asio::error_code &error) {
+    if (!error) {
+      new_connection->start();
+    }
+
+    start_accept();
+  }
+
+  asio::io_context &io_context_;
+  tcp::acceptor acceptor_;
+};
+
+int main() {
+  struct sigaction sigIntHandler;
+
+  sigIntHandler.sa_handler = my_handler;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
+
+  sigaction(SIGINT, &sigIntHandler, NULL);
+
+
   try {
     asio::io_context io_context;
-
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8060));
-    for (;;) {
-      tcp::socket socket(io_context);
-      acceptor.accept(socket);
-      std::string message = make_daytime_string();
-      asio::error_code ignored_error;
-      asio::write(socket, asio::buffer(message), ignored_error);
+    tcp_server server(io_context);
+    infinite_timer timer(io_context);
+    while(!should_stop) {
+      io_context.run_one();
     }
-  } catch (std::exception &ex) {
-    std::cout << ex.what() << std::endl;
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
   }
+
   return 0;
 }
